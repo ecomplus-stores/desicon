9c5377af8a5d62dfed5385905e490d86f1ede65d
matheusgnreis <matheus@e-com.club>
2023-05-03 20:49:44 -0300

    chore: update store with atacado

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -5,7 +5,9 @@ const pathDslAlias = path.resolve(dirSearchAlias, 'dsl')
 module.exports = () => ({
   resolve: {
     alias: {
-      './methods/set-search-term': path.resolve(dirSearchAlias, 'set-search-term')
+      './methods/set-search-term': path.resolve(dirSearchAlias, 'set-search-term'),
+      './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'),
+      './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/js/TheProduct.js'),
     }
   }
 })
