9759bfe8913eddebca86eb952e49e08dcbeeb3fb
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-11-14 04:11:06 -0300

    chore(search-engine): allow more than 10 grid options

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -5,6 +5,9 @@ const pathDslAlias = path.resolve(dirSearchAlias, 'dsl')
 module.exports = () => ({
   resolve: {
     alias: {
+      './lib/dsl': pathDslAlias,
+      './../lib/dsl': pathDslAlias,
+      '../lib/dsl': pathDslAlias,
       './methods/set-search-term': path.resolve(dirSearchAlias, 'set-search-term'),
       './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'),
       './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/js/TheProduct.js'),
