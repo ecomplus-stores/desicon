39802ed364671282f782b8862fd9a12613c72988
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2024-01-29 13:25:42 -0300

    Update storefront.webpack.js

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -10,9 +10,9 @@ module.exports = () => ({
       '../lib/dsl': pathDslAlias,
       './methods/set-search-term': path.resolve(dirSearchAlias, 'set-search-term'),
       './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'),
-      './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/js/TheProduct.js'),
-      './html/TheAccount.html': path.resolve(__dirname, 'template/js/custom-js/html/TheAccount.html'),
-      './js/TheAccount.js': path.resolve(__dirname, 'template/js/custom-js/js/TheAccount.js')
+      './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/js/TheProduct.js')
+      //'./html/TheAccount.html': path.resolve(__dirname, 'template/js/custom-js/html/TheAccount.html'),
+      //'./js/TheAccount.js': path.resolve(__dirname, 'template/js/custom-js/js/TheAccount.js')
     }
   }
 })
