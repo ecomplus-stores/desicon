3a596008729012aa016835327ecbd3af88e9de8a
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2024-02-06 10:46:35 -0300

    chore: update desicon

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -9,6 +9,8 @@ module.exports = () => ({
       './../lib/dsl': pathDslAlias,
       '../lib/dsl': pathDslAlias,
       './methods/set-search-term': path.resolve(dirSearchAlias, 'set-search-term'),
+      './js/ShippingCalculator.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingCalculator.js'),
+      './html/ShippingCalculator.html': path.resolve(__dirname, 'template/js/custom-js/html/ShippingCalculator.html'),
       './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'),
       './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/js/TheProduct.js')
       //'./html/TheAccount.html': path.resolve(__dirname, 'template/js/custom-js/html/TheAccount.html'),
