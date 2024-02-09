bc8323bae08f6a8a2c85ea0b78bcd6aa52088564
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2024-02-08 23:48:26 -0300

    chore: update select service

--- template/js/custom-js/js/ShippingCalculator.js
+++ template/js/custom-js/js/ShippingCalculator.js
@@ -96,6 +96,7 @@ import {
         shippingServices: [],
         selectedService: null,
         hasPaidOption: false,
+        hasFreeOption: false,
         freeFromValue: null,
         isScheduled: false,
         retryTimer: null,
@@ -190,10 +191,17 @@ import {
                     ? -1
                     : 1
             })
-            // this.setSelectedService(0)
             this.hasPaidOption = Boolean(this.shippingServices.find(service => {
               return service.shipping_line.total_price || service.shipping_line.price
             }))
+            this.hasFreeOption = Boolean(this.shippingServices.find(service => {
+              return service.shipping_line.total_price === 0 || service.shipping_line.price === 0
+            }))
+            if (this.hasFreeOption) {
+              this.setSelectedService(1)
+            } else {
+              this.setSelectedService(0)
+            }
             if (Array.isArray(this.shippingAppsSort) && this.shippingAppsSort.length) {
               this.shippingServices = sortApps(this.shippingServices, this.shippingAppsSort)
             }
