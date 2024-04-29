c230ccccb3cdb2e3fc0f9aeb0cf0cc8434e2c115
Vitor Rocha Gonçalves <35348281+vitorrgg@users.noreply.github.com>
2024-04-29 12:18:58 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,7 @@
 // Add your custom JavaScript for storefront pages here.
+const affiliateLinkDiv = document.getElementById('affiliate-link')
+if (affiliateLinkDiv) {
+  import('./components/AffiliateLink.vue').then(({ default: AffiliateLink }) => {
+    new Vue(AffiliateLink).$mount(affiliateLinkDiv)
+  })
+}
